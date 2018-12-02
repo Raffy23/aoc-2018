beda1583a2b511d7232dd6873fe51fe714eada8c
Raphael Ludwig <raphael.ludwig@happywetter.at>
2018-12-02 20:01:46 +0100

    Inital Commit (Quick and Dirty Day 1 and 2)

---
+++ src/main/scala/aoc2018/Day1.scala
@@ -0,0 +1,27 @@
+package aoc2018
+
+import scala.collection.mutable
+import scala.io.Source
+
+/**
+  * Created by: 
+  *
+  * @author Raphael
+  * @version 02.12.2018
+  */
+object Day1 extends App {
+
+  lazy val in = Source.fromResource("day1.txt").getLines().toStream.map(_.toInt)
+  lazy val endless: Stream[Int] = in #::: endless
+  val foundFreq = new mutable.TreeSet[Int]()
+  var foundDuplicate = false
+
+  println("=== AoC - 2018 ===")
+  println(" * Day1: ")
+  println(s"   - Part 1: ${in.sum}")
+  println(s"   - Part 2: ${endless.takeWhile(_ => !foundDuplicate).reduce{ (a,b) =>
+    foundDuplicate = !foundFreq.add(a+b)
+    a+b
+  }}")
+
+}
---
+++ src/main/scala/aoc2018/Day2.scala
@@ -0,0 +1,42 @@
+package aoc2018
+
+import scala.io.Source
+
+/**
+  * Created by: 
+  *
+  * @author Raphael
+  * @version 02.12.2018
+  */
+object Day2 extends App {
+
+  val in = Source.fromResource("day2.txt").getLines().toStream
+
+  println("=== AoC - 2018 ===")
+  println(" * Day2: ")
+
+  var twice = 0
+  var threes = 0
+
+  in.foreach(_.groupBy(identity).mapValues(_.length).values.toSet[Int].foreach{
+    case 2 => twice += 1
+    case 3 => threes += 1
+    case _ =>
+  })
+
+  println(s"   - Part 1: $twice * $threes = ${twice * threes}")
+
+  val minDiff = in.map { str =>
+    val v = in.map(other => (other, str.hammingDiff(other))).filter(_._2 > 0).minBy(_._2)
+    (str, v._1, v._2)
+  }.minBy(_._3)
+
+  println(s"   - Part 2: ${minDiff._1 intersect minDiff._2}")
+
+  implicit class HammingString(val str: String) extends AnyVal {
+    def hammingDiff(other: String): Int = {
+      str.indices.map(idx => other(idx) == str(idx)).count(!_)
+    }
+  }
+
+}
